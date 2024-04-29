95d114e6ec4edd92de25d4e488c8ffbbc9e403de
Tommaso Boccali <tommaso.boccali@cern.ch>
2024-04-29 17:32:59 +0200

    add script

---
+++ fixp7m.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+find . -name \*.p7m  | while read LINE; do 
+    
+    OLD=$LINE 
+    NEW=$(echo $OLD|sed s#\.p7m##g)
+    echo FILES \"$OLD\" \"$NEW\"
+    openssl smime -verify -noverify -in "$OLD" -inform DER -out "$NEW"
+
+done
+
