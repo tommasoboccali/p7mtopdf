92c623a00607a4f8d1474a8e2fe245f032af9f1a
Tommaso Boccali <tommaso.boccali@cern.ch>
2024-04-30 15:23:59 +0200

    Update fixp7m.sh

--- fixp7m.sh
+++ fixp7m.sh
@@ -3,7 +3,7 @@ find . -name \*.p7m  | while read LINE; do
     
     OLD=$LINE 
     NEW=$(echo $OLD|sed s#\.p7m##g)
-    echo FILES \"$OLD\" \"$NEW\"
+    echo Converting  \"$OLD\" to \"$NEW\"
     openssl smime -verify -noverify -in "$OLD" -inform DER -out "$NEW"
 
 done
